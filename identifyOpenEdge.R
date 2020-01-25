ce98b5d476bcedefce533d6e592724d452119684
Potterf <mpotterf@jyu.fi>
2020-01-25 10:33:41 +0200

    working for loop script

--- identifyOpenEdge.R
+++ identifyOpenEdge.R
@@ -4,6 +4,13 @@
 
 # Process:
 # Read shapefile stands data
+# subset one stand
+# identify neighbours: # https://walkerke.github.io/2016/07/spatial-neighbors-in-r---an-interactive-illustration/
+#   - by distance (if gaps between stands...)
+#   - by continuity: rook & queen neigh
+# create surrounding buffer: outside only
+# subset all stands falling in the outside buffer
+# compare tree height between stand in the middle and stands in buffer
 # get fake tree height among polygons
 # calculate medium tree height per stand
 # compare medium tree height with his neighbours:
@@ -17,78 +24,321 @@ rm(list = ls())
 library(ggplot2)  # for choropleth map plot
 library(broom) # to convert spatial data to dataframe
 library(mapproj)
-library(spdep)
+library(spdep)    # neighbours
 library(rgdal)
+library(rgeos)
 library(sf)
 library(raster)
 library(dplyr)
 library(spData)
+library(sf)
+library(RColorBrewer)
 
 
 # create simple reproductible example
 
+r <- raster(nrow=6, ncol=6, crs = "+init=epsg:2957")
+values(r) <- matrix(data = c(9,  NA, NA, NA, NA, NA,
+                             NA, NA, NA, NA, NA, NA, 
+                             NA, NA, 2, 1, 3, 1, 
+                             NA, NA, 1, 1, 1, 1,
+                             NA, NA, 1, 2, 2, 1,
+                             NA, NA, 1, 1, 1, 1),
+                    nrow = 6,
+                    ncol = 6, 
+                    byrow = TRUE)
 
 
-r <- raster(nrow=6, ncol=6)
-values(r) <- matrix(data = c(rep(1,8), 9, rep(1,8), 9), 
-                    nrow = 6, ncol = 6)
+set.seed(5)
+values(r) <- matrix(data = runif(36)*100,
+                    nrow = 6,
+                    ncol = 6, 
+                    byrow = TRUE)
+
+# Convert raster to polygon
+polys <- rasterToPolygons(r)
+
+# Define my colors
+my.palette <- brewer.pal(n = 7, name = "Greens")
+
+
+plot(polys)
+text(polys, 1:length(polys))
+
+
+spplot(polys, 
+       "layer", 
+       col.regions = my.palette,
+       cuts = length(my.palette) - 1,
+       col = "transparent")  # number of cuts smaller by one 
+
+# Check if the cell has a neighbour:
+#  neighbourhood by distance, 
+#                by continuity
+# if no neighbors or open from > = 1 edge: open_edge = TRUE
+# if surrounded by neighbours: 
+# compare the stand heights between neighbours
+#
+
+# -----------------
+#  Neighbors
+# -----------------
+
+# continuity based neighbourhood: 
+# import whole 
+# shapefile, do not split it by one feature at time
+nb <- poly2nb(polys, 
+              #row.names = polys,
+              snap = 10) # snap corrects for the gaps/slivers
+
+# store the number of neighbours by cell
+polys$nb_count<- card(nb)
+
+# Has the stand an open edge? Is surrounded by neighbors, pre-value is FALSE
+polys$open_edge = ifelse(card(nb) <8, "TRUE", "FALSE")
+
+# If has complete neighbors,
+# check the differences in height
+
+# Get the position of the cell surrounded by neighbors
+center.index <- which(polys$nb_count == 8)
+
+# Get the stand height of a stand
+# as a vector to compare element wise
+center.height = polys[center.index,]$layer
+
+
+# Loop through the cells with neighbors:
+# keep height of the central stand
+# get height of neighbors
+# compare the height between them
+# if difference is more than 5: => open_edge = TRUE
+for (i in seq_along(center.index)) {
+  
+  # get index of the stand in a center
+  print(i)
+  print(center.index[i])
+  
+  # Get central stand height 
+  center.height = polys[center.index[i],]$layer
+  print(center.height)
+  
+  # Identify neighbors of the stands
+  # by the index value
+  # print("neighbors are:")
+  nb.index = unlist(nb[center.index[i]])
+  print(nb.index)
+  class(nb.index)
+  # print(nb.index)
+  # Get heights of the stands
+  nb.height = polys[nb.index,]$layer
+  print(nb.height)
+  
+  # Adjust Center.height length as a vector to compare element wise
+  center.height.v = rep(center.height, length(nb.index))
+  (center.height.v)
+  
+  # Compare the heights 
+  h.diff = center.height.v - nb.height
+  print(length(h.diff))
+  
+  center.index[i]
+  # if any diference is more than 5
+  # if there is not a difference in height, change open_edge = TRUE
+  # value to "TRUE"
+  if (any(h.diff > 5)) {
+    polys@data[center.index[i],]$open_edge <- "TRUE"
+  }
+}
+
+polys@data
+
+
+
+
+
+
+
+
+
+
+
+
+
+polys[center.index[1],]$open_edge <- "TRUE"
+# Get list of neighbours
+nb.index = nb[[center.index]]
+
+# Get heights of the stands
+nb.height = polys[nb.index,]$layer
+
+# Compare the heights 
+h.diff = center.height - nb.height
+
+# if there is not a difference in height, change open_edge = TRUE
+# value to "TRUE"
+if (any(h.diff > 5)) {
+  polys@data[6,]$open_edge <- "TRUE"
+}
+
+
+spplot(polys, #"layer", 
+       col.regions = my.palette,
+       cuts = length(my.palette) - 1,
+       col = "transparent")  # number of cuts smaller by one 
+
+
+
+polys@data
+
+
+
+
+# ----------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# convert to full binary adjacency matrix
+mm = nb2mat(nb, zero.policy = TRUE, style = "B")
+
+
+
+isTRUE(all.equal(nb, polys, check.attributes = FALSE))
+
+windows(height = 3, width = 6)
+par(mfrow = c(1,2))
+
 
 plot(r)
+plot(polys, add = T)
+rgeos::polygonsLabel(polys, labels = row.names(polys))
 
-# Conevrt raster to polygon
-pols <- rasterToPolygons(r)
 
-plot(pols, add = T)
 
-# Check if the cell has a neighbour, 
-# if yes, add this attribute
-# pseudocode:
-# subset one cell & remove from others 
-# check if cell touches neighbours
-# subset those cells
-# compare 
+
+plot(polys, border = "grey", lwd = 0.5)
+plot(nb, coordinates(polys), points=FALSE, add = T, lwd = 0.7)
+
+# extract the number of neighbours
+card(nb)
+
+# add number of neighbours to original polygon
+polys$nb_count<- card(nb)
+
+# check the quality of the objects
+summary(nb)
+
+# 
+summary(nb, coordinates(polys))  # if coordinates are provided
+#
+
+
+# --------------------------
+# rgeos::gTouches
+# --------------------------
+
 
 # Subset first row in SpatialPolygonDataFrame
-
-
-one = pols[5, ]
+i = 1
+one = polys[i, ]
 
 # Keep the remaining polygons
-left = pols[-5,]
-
-# Spatially subset the neighbours
-gTouches(sp::geometry(one),
-         sp::geometry(left), 
-         byid = TRUE)  # compare one by one
+left = polys[-i,]
 
 nbrs <- left[which(gTouches(sp::geometry(one),
                                sp::geometry(left), 
                                byid = TRUE)),]
 
 # Compare if the values are different
-height.one = rep(one$layer[1], nrow(nbrs))
+height.one  = rep(one$layer[1], nrow(nbrs))
 height.nbrs = nbrs$layer
 
 # Get the differences between the neighbouring stands
 difference = height.one - height.nbrs
 
-# If the difference in more than 5, set open_edge = TRUE 
+# If the difference in at least one stand is 
+# in more than 5, set open_edge = TRUE 
+# or if no neighbours find
 one$open_edge <- any(difference > 5)
 
 polUpd = one
 
 
-# apply a for loop: update the open_edge value for all stands
-polUpd<-SpatialPolygonsDataFrame()
 
-for (i in seq_along(pols)) {
+
+
+# # --------------------------
+# rgeos::buffer and intersect
+# --------------------------
+
+
+# Subset first row in SpatialPolygonDataFrame
+i = 10
+one = polys[i, ]
+
+# Keep the remaining polygons
+left = polys[-i,]
+
+# Create buffer within distance
+buff = buffer(one, width = 100)
+
+
+
+# subset set of neighbours by spatial overlap
+nbrs <- left[which(gContains(sp::geometry(buff),
+                                    sp::geometry(left), byid = T)),]
+
+
+# Compare if the values are different
+height.one  = rep(one$layer[1], nrow(nbrs))
+height.nbrs = nbrs$layer
+
+# Get the differences between the neighbouring stands
+difference = height.one - height.nbrs
+
+# If the difference in at least one stand is 
+# in more than 5, set open_edge = TRUE 
+# or if no neighbours find
+one$open_edge <- any(difference > 5)
+
+
+# 
+polUpd = one
+
+
+
+plot(polys)
+plot(left, add = T, col = "green")
+plot(one, add = T, col = "red")
+plot(buff, add = T, col = "blue")
+plot(nbrs, add = T, col = "orange")
+
+
+
+
+
+
+for (i in seq_along(polys)) {
+  
   print(i)
   
   # subset one polygon
-  one = pols[i, ]
+  one = polys[i, ]
   
   # Keep the remaining polygons
-  left = pols[-i,]
+  left = polys[-i,]
   
   # Spatially subset the neighbours
   gTouches(sp::geometry(one),
@@ -140,6 +390,45 @@ plot(nbrs, add = T, col = "yellow")
 
 
 
+
+
+
+
+
+# -----------------------------------
+
+# spatial overlay
+
+
+
+# ----------------------------------
+
+nb.cover <- left[gCovers(sp::geometry(buff),
+                         sp::geometry(left),
+                         byid = TRUE),]
+
+nb.over<- left[over(sp::geometry(buff),
+                    sp::geometry(left)),]
+
+class(over(buff, left))
+
+
+
+nb.contains <- left[which(gContainsProperly(sp::geometry(buff),
+                                            sp::geometry(left), byid = T)),]
+
+
+gCoveredBy(sp::geometry(buff),
+           sp::geometry(left), byid = T)
+
+
+
+nbrs <- left[which(over(sp::geometry(buff),
+                        sp::geometry(left), 
+                        byid = TRUE)),]
+
+
+# --------------------------------
 
 
 
