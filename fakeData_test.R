7f79c3f1f91a4cb445f91b54105e5aef7ad64a49
Potterf <mpotterf@jyu.fi>
2020-03-13 12:37:13 +0200

    make example for stackoverflow

--- fakeData_test.R
+++ fakeData_test.R
@@ -34,25 +34,100 @@ one  = my.sf.web[i, ]
 left = my.sf.web[-i,]
 
 # Create buffer 
-buff = st_buffer(one, 35000 ) # distance
-
+buff35 = st_buffer(one, 35000 ) # distance
+buff50 = st_buffer(one, 50000)
 
 # CHeck which polygons overlaps with my buffer
 out.overlap = st_overlaps(buff, left)
 
 
 # Subset the polygons that overlaps with the buffer
-nbrs.buff <- left[st_overlaps(buff,left)[[1]],]
+nbrs.buff35 <- left[st_overlaps(buff35,left)[[1]],]
+nbrs.buff50 <- left[st_overlaps(buff50,left)[[1]],]
+
+
 
 # Merge together `neighbors`` and `one`
 u <- st_union(rbind(nbrs.buff, one))
 
-#u <- st_union(st_geometry(nbrs.buff), st_geometry(one), by_feature = FALSE)
-#u <- st_union(st_combine(st_geometry(nbrs.buff)), 
-#             st_combine(st_geometry(one)))
 int.buff.one = st_difference(buff, u) 
 
 int.buff.one.area <- st_area(int.buff.one)
 
+library(gridExtra)
 
+p1 <- ggplot(nbrs.buff50) +
+  geom_sf(fill = "lightblue") +
+  geom_sf(data = buff50, fill = NA, col = "red", lwd = 1)
+  
+p2 <- ggplot(nbrs.buff35) +
+  geom_sf(fill = "grey") +
+  geom_sf(data = buff35, fill = NA, col = "red", lwd = 1)
+
+grid.arrange(p1, p2,  nrow = 1)
+
+
+
+
+# ---------------------------------------------------
+
+# Why overlay operation does not subset the completely overlapped features?
+# ----------------------------------------------------
+
+
+# Load data
+shp = system.file("shape/nc.shp", package="sf")
+
+my.sf <- st_read(shp, quiet = TRUE)
+
+# Convert crs to projected system to make buffer
+my.sf.web<- st_transform(my.sf, 3857)
+
+# Subset the data to create two independent shps
+i = 10
+
+# Split datasets in two files
+one  = my.sf.web[i, ]
+#left = my.sf.web[-i,]
+
+# Create buffer 
+#buff35 = st_buffer(one, 35000 ) # distance
+buff50 = st_buffer(one, 50000)
+
+# CHeck which polygons overlaps with my buffer
+out.overlap = st_overlaps(buff50, my.sf.web)
+
+out.overlap <- my.sf.web[st_intersects(buff50, my.sf.web, sparse =  FALSE),]
+
+
+
+# Subset the polygons that overlaps with the buffer
+#nbrs.buff35 <- left[st_overlaps(buff35,left)[[1]],]
+nbrs.buff50 <- my.sf.web[st_overlaps(buff50,my.sf.web)[[1]],]
+#nbrs.buff50 <- my.sf.web[st_covers(buff50,my.sf.web)[[1]],]
+nbrs.buff50 <- my.sf.web[st_within(buff50,my.sf.web)[[1]],]
+
+
+
+library(gridExtra)
+
+ggplot(buff50) +
+  geom_sf(data = my.sf.web, fill = "lightgrey") +
+    geom_sf(data = nbrs.buff50, fill = "red") +
+    geom_sf(fill = NA, col = "green", lwd = 2) +
+  geom_sf(data = out.overlap, fill = "violet")
+  
+
+ggplot(out.overlap) +
+  geom_sf(data = out.overlap) +
+  geom_sf(data = buff50, col = "red", fill = NA)
+
+
+p2 <-
+  ggplot(nbrs.buff50) +
+  geom_sf(fill = "lightgrey") +
+  geom_sf(data = buff50, fill = NA, col = "red", lwd = 1)
+
+
+grid.arrange(p1, p2,  nrow = 1)
 
