08772a2af0b3682b3d50a016225256bd8687df7f
Potterf <mpotterf@jyu.fi>
2020-03-30 15:30:45 +0300

    how to verify that my output data are correct? can I use Suvanto's model directly?

--- glm_input_execute.R
+++ glm_input_execute.R
@@ -47,9 +47,6 @@ setwd(inDataPath)
 # # read simulated data
 df <- read.csv("rsl_without_MV_Korsnas.csv", sep = ";")  # without == climate change is not included
 # 
-# # Read stand geometry b!!!needs to be changed!!!
-#df.geom = read_sf("U:/projects/2019_windthrowModel/Janita/outSimulatedoutKorsnas_att.shp",
- #                 as_tibble = FALSE)
 
 # st_read do not return tibble just spatial dataframe
 df.geom = st_read("outKorsnas_att.shp")
@@ -58,7 +55,10 @@ df.geom = st_read("outKorsnas_att.shp")
 # CHeck attributes that I have:
 names(df.geom)
 
-keep<- c("standid", "maingrp", "subgrop", "soiltyp", "area",
+keep<- c("standid", 
+         "mntrspc",   
+         "soiltyp", 
+         "area",
          "avgTemp",
          "windSpd",
          "solDpth",
@@ -73,7 +73,7 @@ df.geom <- df.geom %>%
 stands.complete = Reduce(intersect, list(df$id, df.geom$standid))
 
 # Reduce df and df.geom to the same stands
-df <- subset(df,id %in% stands.complete)
+df      <- subset(df,id %in% stands.complete)
 df.geom <- subset(df.geom, standid %in% stands.complete)
 
 
@@ -84,22 +84,46 @@ df.bau<-
   mutate(regime = factor(regime))        # drop unused factors
 
 
-# Merge geometry and simulated BAU data
+# Merge geometry and simulated data for one management regime
 stand.merged <- sp::merge(df.geom,
                           df.bau, 
                           duplicateGeoms = TRUE,
-                          by.x = "standid", by.y = "id")
+                          by.x = "standid", 
+                          by.y = "id")
 
 
+# -----------------------------------
+#
+# Reorganize the input data to fit Suvanto model's requirement
 # !!!! Add missing columns:
 # create fake data!!!!
+# variables used fromSIMO instead from rasters:
+# 
+# SC - identify fetility class
+# SOIL_CLASS - identify coarsiness of teh soil ^ soil_depth <30 TRUE/FALSE
+# PEAT - identify where is peat
+# 
 # -----------------------------------
 stand.merged$time_thinning <- factor(sample(c("0-5", "6-10", ">10"),
                                         nrow(stand.merged), replace = TRUE), 
                                  levels = c("0-5", "6-10", ">10"))
-stand.merged$soiltyp <- sample(c("mineral coarse", "mineral fine",
+
+stand.merged$soiltyp <- factor(sample(c("mineral coarse", 
+                                 "mineral fine",
                                  "organic"),
-                               nrow(stand.merged), replace = TRUE)
+                               nrow(stand.merged), 
+                               replace = TRUE),
+                               levels = c("mineral coarse", 
+                                          "mineral fine",
+                                          "organic")) 
+
+# replace NA values - due to centroids not overlapping with raster
+stand.merged$slFrtlC <- replace_na(stand.merged$slFrtlC, "poor")
+
+# Soil depth: TRUE & FALSE
+# soil depth < 30: 1 = TRUE, 0 = FALSE
+stand.merged$solDpth <- ifelse(stand.merged$solDpth == 0, FALSE, TRUE)
+
 
 # Subset columns crucial for glm()
 my.cols.glm <- c("standid",
@@ -114,26 +138,21 @@ my.cols.glm <- c("standid",
                  "slFrtlC", 
                  "avgTemp")
 
-# Sprocess teh data: change species numbers, 
-# change NA in H_dom to 0
-# !!!!! heck the conversion to factor!!!
-
+# Replace the species values
+# seems that these data have only pine???
+# mainGRp is land type == forest = 1
+# need to use MAIN_SP from simulated data !! 
+# or mntrspc
 
 stand.merged<-
   stand.merged %>% 
-  mutate(species2 = as.character(maingrp)) %>% 
-  mutate(species = case_when(
-    species2 == "1" ~ "pine",
-    species2 == "2" ~ "spruce",
-    TRUE ~ "other")) %>% 
-  mutate(H_dom = replace_na(H_dom, 0)) %>% 
-  mutate(species = as.factor(species))
+  mutate(species = case_when(mntrspc == 1 ~ "pine",
+                             mntrspc == 2 ~ "spruce",
+                             TRUE ~ "other")) %>% 
+  mutate(H_dom = replace_na(H_dom, 0.1)) %>%   # no possible to get log(0)
+  dplyr::select(my.cols.glm)  %>%      # select columns 
+  mutate_if(is.character, as.factor)   # convert all characters to factor
   
-# filter the columns
-stand.merged <-
-  stand.merged %>%
-  dplyr::select(my.cols.glm)
-
 
 # subset just one year
 stand.merged.2016 <- 
@@ -141,35 +160,98 @@ stand.merged.2016 <-
   filter(year == 2016) 
 
 
-
 # Calculate open edge:
-stand.merged.2016<- findOpenEdge_sf(stand.merged.2016, H_dom, distance = 40, pixel.width = 16)
+stand.merged.2016<- findOpenEdge_sf(stand.merged.2016, 
+                                    H_dom, 
+                                    distance = 40, 
+                                    pixel.width = 16)
 
 
 
-# Restructure the data into categorical and quantitative data
 # ----------------------------------------------
-# Convert categorical into binary
-categVars <- c("species", 
-               "time_thinning", 
-               "open_stand",
-               "soiltyp", 
-               "solDpth",
-               "slFrtlC")
+# Restructure the data into categorical 
+#        and quantitative data
+#              for glm()
+# ----------------------------------------------
 
 
 
 # Convert spatial file to NULL geometry = normal dataframe
-st_geometry(stand.merged.2016) <- NULL
+df<-stand.merged.2016
+st_geometry(df) <- NULL  # get rid of geometry
+
 
 library(fastDummies)
 
-df.bin <- fastDummies::dummy_cols(stand.merged.2016,
+# =========================
+# fake data
+# Check if the model works???
+# ---------------------
+
+set.seed(42)
+
+row.num = 80
+
+species         <- factor(rep(c("pine", "spruce", "other"),
+                              each = row.num), 
+                          levels = c("pine", "spruce", "other"))
+height          <- c(runif(row.num, min = 10, max = 200),
+                     runif(row.num, min = 0, max = 100),
+                     runif(row.num, min = 30, max = 150))
+time_thinning   <-  factor(sample(c("0-5", "6-10", ">10"),
+                                  length(species), replace = TRUE), 
+                           levels = c("0-5", "6-10", ">10"))
+windSpeed       <- runif(length(species), min = 10, max = 30)
+open_stand      <- ifelse(rbinom(length(species), 1, 0.9), "FALSE", "TRUE") 
+soilType        <- sample(c("mineral coarse", "mineral fine",
+                            "organic"),
+                          length(species), replace = TRUE)
+soilDepthLess30 <- ifelse(rbinom(length(species), 1, 0.5),
+                          "FALSE","TRUE")
+siteFertility   <- factor(sample(c("poor", "fertile"),
+                                 length(species),
+                                 replace = TRUE),
+                          levels = c("poor", "fertile"))
+tempSum         <- runif(length(species), min = 6, max = 16)
+#damageDensity  <- rep(0, length(species))
+
+wind_damage     <- rbinom(length(species), 1, 0.4)
+
+
+# put data together
+df<-data.frame(species, 
+               height,
+               time_thinning,
+               windSpeed,
+               open_stand,
+               soilType,
+               soilDepthLess30,
+               siteFertility,
+               tempSum,
+               wind_damage)
+
+
+
+# convert my categorial variables into binary classes
+# create vector of colnames with categorical variables
+categVars <- c("species", 
+               "time_thinning", 
+               "open_stand",
+               "soilType", 
+               "soilDepthLess30",
+               "siteFertility")
+
+
+
+library(fastDummies)
+
+df.bin <- fastDummies::dummy_cols(df,
                                   select_columns = categVars, # only categorical
                                   remove_first_dummy = TRUE)  # remove reference category
 
 # remove the original variables and observed value of damages
-df.bin<-df.bin[ , !(names(df.bin) %in% c(categVars))]
+df.bin<-df.bin[ , !(names(df.bin) %in% c(categVars,
+                                         "wind_damage"))]
 
 
 # complete the dataframe by interactions 
@@ -178,10 +260,8 @@ df.bin<-df.bin[ , !(names(df.bin) %in% c(categVars))]
 # add columnf for intersectp => fill with 1
 df.bin$interc <- 1
 
-df.bin$log_height <- log(df.bin$H_dom)
-df.bin$log_Wspeed <- log(df.bin$windSpd)
-
-
+df.bin$log_height <- log(df.bin$height)
+df.bin$log_Wspeed <- log(df.bin$windSpeed)
 
 # add interactions
 df.bin$spec.spruce.X.log.height <- df.bin$species_spruce * df.bin$log_height
@@ -189,35 +269,124 @@ df.bin$spec.other.X.log.height  <- df.bin$species_other  * df.bin$log_height
 
 
 
+# to put it in correct order
+colnames.ordered<-c("interc",
+                    "species_spruce",
+                    "species_other",
+                    "log_height",
+                    "time_thinning_6-10", 
+                    "time_thinning_>10",
+                    "log_Wspeed",
+                    "open_stand_TRUE",
+                    "soilType_mineral fine",
+                    "soilType_organic",
+                    "soilDepthLess30_TRUE",
+                    "siteFertility_fertile",
+                    "tempSum",
+                    "spec.spruce.X.log.height",
+                    "spec.other.X.log.height" )
+
+# order the dataframe to corresponds columnwise to coefficients
+# keep only specified columns
+df.ord<-df.bin[colnames.ordered]
+
+
+# calculate partial df 
+# the final column need to be summed up
+part.df <- sweep(df.ord, 2, suvantoCoeffs, "*")
+
+# sum by rows and add intercept value
+df.ord$pred.manual <- logit2prob(rowSums(part.df))
+
+
+# Can I directly import Suvanto's model?
+# Thaen I can use just predict() function, 
+# no need to split categorical data into binary
+
+# how to know if my probability values are correctly calculated?
+# try to subset Suvanto's raw data and again evaluate?
 
 
 
 
 
 
-# order the columns: 
-# Reorder the columns:
-# add bo coefficient
-stand.merged.2016$b0 <- rep(1, nrow(stand.merged.2016)) 
 
-stand.merged.2016 <-stand.merged.2016["standid",
-                                      "area",
-                                      "year",
-                                      "b0",
-                                      "species",
-                                      "H_dom", 
-                                      "time_thinning",
-                                      "windSpd",
-                                      "open_edge",
-                                      "soiltyp",         
-                                      "solDpth",
-                                      "slFrtlC", 
-                                      "avgTemp"]
-  
-]
 
-my.cols.glm.ord <- c()
+# -----------------------------------
+# my data
+# ---------------------------------
 
+library(fastDummies)
+
+
+# select categorical data
+categVars <- c("species", 
+               "time_thinning", 
+               "open_edge",
+               "soiltyp", 
+               "solDpth",
+               "slFrtlC")
+
+
+# Define correct level order: 
+df$species   <- factor(df$species,   levels = c("pine", "spruce", "other"))
+df$solDpth   <- factor(df$solDpth,   levels = c("FALSE", "TRUE"))
+df$open_edge <- factor(df$open_edge, levels = c("FALSE", "TRUE"))
+df$slFrtlC   <- factor(df$slFrtlC,   levels = c("poor", "fertile"))
+
+# all my stands are just pine!!! this makes different table output
+df.bin <- fastDummies::dummy_cols(df,
+                                  select_columns = categVars, # only categorical
+                                  remove_first_dummy = TRUE)  # remove reference category
+
+# remove the original variables and observed value of damages
+df.bin<-df.bin[ , !(names(df.bin) %in% c(categVars))]
+
+
+# Complete missing columns:
+#  - intercept
+#  - logarithms
+#  - interactions
+# Need to have the same number of columns as coefficients
+df.bin$interc     <- 1
+df.bin$log_height <- log(df.bin$H_dom)
+df.bin$log_Wspeed <- log(df.bin$windSpd)
+
+
+# add interactions
+df.bin$spec.spruce.X.log.height <- df.bin$species_spruce * df.bin$log_height
+df.bin$spec.other.X.log.height  <- df.bin$species_other  * df.bin$log_height
+
+
+names(df.bin)
+
+
+# ---------------------------
+#     Order the columns: 
+# ---------------------------
+
+# to put it in correct order
+colnames.ordered<-c("interc",
+                    "species_spruce",
+                    "species_other",
+                    "log_height",
+                    "time_thinning_6-10", 
+                    "time_thinning_>10",
+                    "log_Wspeed",
+                    "open_edge_TRUE",
+                    "soiltyp_mineral fine",
+                    "soiltyp_organic",
+                    "solDpth_TRUE",
+                    "slFrtlC_fertile",
+                    "avgTemp",
+                    "spec.spruce.X.log.height",
+                    "spec.other.X.log.height" )
+
+
+# order the dataframe to corresponds columnwise to coefficients
+# keeps only specified columns
+df.ord<-df.bin[colnames.ordered]
 
 
 # ---------------------------------
@@ -262,6 +431,29 @@ suvantoCoeffs<-c(intercept,
                  b14.spec.other.X.log.height)
 
 
+
+# Multiple the dataframe by vector
+# calculate partial df 
+# the final column need to be summed up
+part.df <- sweep(df.ord, 2, suvantoCoeffs, "*")
+
+
+# Function: convert logit to probability
+# Convert logit to probabilities
+logit2prob <- function(logit){
+  odds <- exp(logit)
+  prob <- odds / (1 + odds)
+  return(prob)
+}
+
+
+# Convert sum of `y.logit` values to probability values
+# sum by rows and add intercept value
+df.ord$pred.manual <- logit2prob(rowSums(part.df))
+
+#####
+
+###!!!! predicted values are all equal 1 !!! What's is the problem???
 
 
 
