deb6abda86f8827969a61cbd318936fa044fbc86
Potterf <mpotterf@jyu.fi>
2020-04-16 13:28:01 +0300

    subset optimal management regimes

--- reconstruct_optimal.R
+++ reconstruct_optimal.R
@@ -17,3 +17,62 @@
 
 
 
+rm(list = ls())
+
+library(sf)
+
+
+
+
+# Use new dataset
+# --------------------------------
+myPathAll = paste(getwd(), "input", sep = "/")
+setwd(myPathAll)
+
+
+# Use new values 
+# =================
+# Read only files containing _NO_ = no wind scenario
+# ^ anchor the beginning (if you begin with power, you end up with money (Evan Misshula, ))
+# https://r4ds.had.co.nz/strings.html#basic-matches
+df.names = list.files(myPathAll,
+                        pattern=".csv$") # ends with .csv$, \\. matches .
+
+# Read all dtaframes in a loop
+my.df.list = lapply(df.names, function(x) {read.csv(x, sep = ";")})
+df = do.call("rbind", my.df.list)
+
+# get the unique stands
+my.stands <- unique(df$id)
+
+# Read stand geometry
+
+# st_read do not return tibble just spatial dataframe
+#df.geom = st_read("stands.shp")
+# I can just subset teh stands again by their stand id from the 
+# petajavesi gpkg???
+df.geom.all <- read_sf("C:/MyTemp/2019_mapAvohaakut/raw/MV_Petäjävesi/MV_Petäjävesi.gpkg",
+                       layer = "stand")
+
+
+# subset the stand geometry
+df.geom.sub<- subset(df.geom.all, standid %in% my.stands)
+
+
+# Get to know the data:
+# -------------------------------
+# how many unique stands?? ~ 1475?? 
+length(unique(df$id))  # YES
+
+
+# how many regimes??
+length(unique(df$regime))
+# only 24 regimes, why???
+
+
+
+
+
+
+
+
