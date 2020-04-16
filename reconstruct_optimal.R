982fe0f9d9dacf36a0be325ab1d03bdeaa75336b
Potterf <mpotterf@jyu.fi>
2020-04-16 11:04:01 +0300

    new script to filter simulated data by optimal solutions

---
+++ reconstruct_optimal.R
@@ -0,0 +1,19 @@
+
+# 
+# Recreate complex landscape for the optimal solutions
+# 
+
+# Read .csv files of optimal solutions
+# if there is not 1.0 solution: 
+# keep only stand that has higher proportion of the regime
+# Based on the scenarios, subset the individual simulated stands
+# ----------------------------------
+
+# Need to:
+# read the .csv  files from simulated data
+# read the optimal solution
+# filter the simulated data by solutions
+# read the stand geometry, calculate open_edge and wind risk
+
+
+
