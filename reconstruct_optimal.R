31c29db9ec7560f5bdc31df51e0bb757b156f26d
Potterf <mpotterf@jyu.fi>
2020-04-16 16:22:08 +0300

    read geometry & csv files, need to read optimal file

--- reconstruct_optimal.R
+++ reconstruct_optimal.R
@@ -22,42 +22,50 @@ rm(list = ls())
 library(sf)
 
 
-
-
-# Use new dataset
-# --------------------------------
-myPathAll = paste(getwd(), "input", sep = "/")
+myPathAll = "C:/MyTemp/myGitLab/windDamage/input"
 setwd(myPathAll)
 
 
-# Use new values 
-# =================
-# Read only files containing _NO_ = no wind scenario
 # ^ anchor the beginning (if you begin with power, you end up with money (Evan Misshula, ))
 # https://r4ds.had.co.nz/strings.html#basic-matches
 df.names = list.files(myPathAll,
-                        pattern=".csv$") # ends with .csv$, \\. matches .
+                        pattern="RCP*.csv$") # ends with .csv$, \\. matches .
 
-# Read all dtaframes in a loop
-my.df.list = lapply(df.names, function(x) {read.csv(x, sep = ";")})
+# Read all dataframes in a loop
+my.df.list = lapply(df.names, function(x) {
+                     read.csv(x, sep = ";")
+  }
+  )
+
 df = do.call("rbind", my.df.list)
 
 # get the unique stands
 my.stands <- unique(df$id)
 
-# Read stand geometry
 
+# Read stand geometry
+# -----------------------------
 # st_read do not return tibble just spatial dataframe
-#df.geom = st_read("stands.shp")
 # I can just subset teh stands again by their stand id from the 
-# petajavesi gpkg???
+# petajavesi gpkg??? Joo!
 df.geom.all <- read_sf("C:/MyTemp/2019_mapAvohaakut/raw/MV_Petäjävesi/MV_Petäjävesi.gpkg",
                        layer = "stand")
 
-
-# subset the stand geometry
+# subset the stand geometry to simulated stands
 df.geom.sub<- subset(df.geom.all, standid %in% my.stands)
 
+# Plot the data
+#plot(df.geom.sub["maingroup"])
+
+#plot(df.geom.all["maingroup"])
+
+
+# Read optimal solution:
+optim <- read.csv("C:/MyTemp/avohaakut_db/solutions/Bundles_2_nocow_NPV_MANAGE_price_three_0_0_1_1_ALL0.csv",
+                  sep =  "\t",
+                  header = F)
+
+optim$V1 <- gsub('(', "", optim$V1)
 
 # Get to know the data:
 # -------------------------------
