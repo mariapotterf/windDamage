16a873db0f1e5fc5169684bf0fb0796b03f526cc
Potterf <mpotterf@jyu.fi>
2020-12-02 18:04:18 +0200

    merged data together

--- manuscript_regimes/n1_putDataTogether.r
+++ manuscript_regimes/n1_putDataTogether.r
@@ -29,13 +29,6 @@ require(data.table)
 library(tidyr)
 
 
-# stands that are not simulated
-stands.remove <-c(13243875, 
-                  13243879, 
-                  13243881,
-                  6685176,     # # H_dom is >150 m 
-                  13243960)    #  H_dom is 430
-
 
 
 # Read corrected simulated names:
@@ -43,18 +36,16 @@ df<- data.table::fread("C:/MyTemp/avohaakut_db/analyzed/simulated_AVK_regimes.cs
                            data.table=FALSE, 
                            stringsAsFactors = FALSE)
 
-df <- subset(df, !id %in% stands.remove)
-
-# Subset stands only for normal H_dom values
-df <- subset(df, !id %in% stands.remove)
-
-
-# stands geometry
+# Stands geometry
 df.geom <- st_read("C:/MyTemp/avohaakut_db/14.534/14.534/mvj_14.534.shp")
 df.geom <- subset(df.geom, select = c("KUVIO_ID"))
 names(df.geom) <- c("id", "geometry")
-df.geom$area <- st_area(df.geom)
-df.geom <- subset(df.geom, id %in% unique(df$id))
+
+
+# Enviro raster derived data
+df.rst <- data.table::fread("C:/MyTemp/myGitLab/windDamage/output/df_glm_raster.csv", 
+                            data.table=FALSE)
+
 
 
 
@@ -67,7 +58,7 @@ unique(df$branching_group)  # this has not correct name of regime
 unique(df$avohaakut)  # avohaakut is correct!!!
 
 # Check how many stands has each regime
-df.sim %>% 
+df %>% 
   group_by(avohaakut) %>% 
   distinct(id) %>% 
   tally() %>% 
@@ -76,32 +67,110 @@ df.sim %>%
 # Some regimes have less rows
 # count number of unique stands 
 
-
-# How to make sure that every regime will have the same stands???
-# filter ids of TT regimes, check what happens if I filter just those 
-stand.ids <- 
-  df %>% 
-  filter(avohaakut == "LRH30") %>% 
-  distinct(id)    # 941 stands
-  
+# Filter all stands having teh same regime
+df.sub <-
+  df %>%
+  group_by(id) %>%
+  filter(n_distinct(avohaakut) == n_distinct(df$avohaakut))
 
 
-# filter the stands to see how many stands i will have by regime
-df.sim %>% 
+# Cehck how many regimes I have
+df.sub %>% 
   group_by(avohaakut) %>% 
-  filter(id %in% stand.ids$id) %>% 
   distinct(id) %>% 
   tally() %>% 
   print(n = 70)
 
 
-# check if every regime occurs over all years?
-df.sim %>% 
-  group_by(avohaakut) %>% 
-#  filter(id %in% stand.ids$id) %>% 
-  distinct(year) %>% 
-  tally() %>% 
-  print(n = 70)
+
+# get vector of stands to filter them out
+shared.stands <- unique(df.sub$id)
+
+
+# Filter simulated data, geometry data and envirodata by vector of ids
+# -----------------------------------------
+df.geom.sub <-
+  df.geom %>% 
+  filter(id %in% shared.stands)
+
+df.rst.sub <-
+  df.rst %>% 
+  rename(id = standid) %>% 
+  filter(id %in% shared.stands) 
+
+
+# Merge datasets together
+# ------------------------------------
+
+# Merge the datasets
+df.out <- df.sub %>%
+  left_join(df.rst.sub, by = ("id")) 
+
+
+# Interpret THIN
+
+# ----------------------------------
+# Interpret of THIN years:
+# ------------------------------
+# convert 0 to NA
+# IN CCF: years area stored as "2016-04-16": 
+# keep only first 4 characters to convert this to numeric values
+# i.e. "2016-04-16" -> to "2016""
+# to calculate yearly differences
+
+df.out1 <- 
+  df.out %>%
+  mutate(THIN = na_if(THIN, 0))  %>% 
+  mutate(THIN2 = substring(THIN,0,4)) %>%  # keep the first 4 characters from CCF regimes, datum in format "2016-04-16" -> to "2016"
+  
+  group_by(id, avohaakut) %>% 
+  mutate(THIN_filled_lagged = lag(THIN2)) %>%
+  mutate(THIN_filled_lagged = as.numeric(THIN_filled_lagged)) %>%
+  tidyr::fill(THIN_filled_lagged) %>% 
+  mutate(difference = year - THIN_filled_lagged) %>% 
+  mutate(since_thin = case_when(is.na(difference) | difference < 0 ~ ">10",
+                                difference %in% c(0:5) ~ "0-5",
+                                difference %in% c(6:10) ~ "6-10",
+                                difference > 10 ~ ">10")) %>% 
+  # Remove unnecessary columns
+  dplyr::select(-branching_group, 
+                -regime.x,
+                -regime.y)
+
+
+
+# # ---------------------------------------
+# Test if THIN is correct for CCF and RF???
+# -------------------------------------
+
+# test if it ok well calculated??
+# Subset two regimes and recalculate teh THIN values:
+df.out1 %>% 
+  filter(id == 13239722 &  avohaakut == "LRT30") %>%   # (avohaakut == "CCF_3_45" )) %>%  # | avohaakut == "LRT30"
+  arrange(year) %>% 
+  dplyr::select(id, year, THIN, THIN_filled_lagged, difference, avohaakut, since_thin) #%>%
+
+
+
+
+
+
+
+# Create two regimes: SA and non-SA"
+df.all <- 
+  df.all %>% 
+  mutate(twoRegm = case_when(avohaakut == "SA" ~ "SA",
+                             avohaakut != "SA" ~ "no_SA"))
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -122,6 +191,12 @@ dd %>%
   group_by(group) %>% 
   filter(all(val))
 
+# Group by IDs and then check which ids have all groups
+dd %>%
+  group_by(val) %>%
+  filter(n_distinct(group) == n_distinct(dd$group))
+
+
 
 #group   val
 #<fct> <dbl>
@@ -132,3 +207,6 @@ dd %>%
 
 
 # Ask Kyle if it is somehow possible to have the same regimes for all stands?
+
+# Read and merge enrivromnetal conditions
+# --------------------------------------------
