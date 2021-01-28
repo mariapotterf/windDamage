52ffcda152472fd5654806cb9a9e7a08cb16ac54
Potterf <mpotterf@jyu.fi>
2021-01-28 10:53:49 +0200

    created table of neighbors and central cell by landscape to compare them later on

--- cleanData/n3_calc_open_edge.r
+++ cleanData/n3_calc_open_edge.r
@@ -26,11 +26,20 @@ stands.remove <- c(13243875,
 
 
 # read optimal simulated data
-df<- data.table::fread("C:/MyTemp/myGitLab/windDamage/output/df_sim_opt.csv",
+df<- data.table::fread("C:/MyTemp/myGitLab/windDamage/output/even_flow/df_sim_opt.csv",
                        data.table=FALSE, 
                        stringsAsFactors = FALSE)
 
+
+
+#df<- data.table::fread("C:/MyTemp/myGitLab/windDamage/output/df_sim_opt.csv",
+ #                      data.table=FALSE, 
+  #                     stringsAsFactors = FALSE)
+
+
+
 df <- subset(df, !id %in% stands.remove)
+
 
 keep_id <- unique(df$id)
 
@@ -42,6 +51,17 @@ names(df.geom) <- c("id", "geometry")
 # df.geom$area <- st_area(df.geom)
 df.geom <- subset(df.geom, !id %in% stands.remove)
 df.geom <- subset(df.geom, id %in% keep_id)
+
+
+
+# Replace H_dom if NA = 0 --------
+df <- df %>% 
+  dplyr::mutate(H_dom  = replace_na(H_dom, 0)) %>% 
+  mutate(scenario = gsub("./Bundles_2_nocow_INCOME_MANAGE_price_three_0_0_1_1", '', scenario)) %>% 
+  mutate(landscape = gsub("./Bundles_2_nocow_INCOME_MANAGE_price_three_0_0_1_1", '', landscape)) #%>% 
+  
+
+
 
 
 
@@ -62,6 +82,26 @@ land.ls <- df %>%
   group_by(landscape) %>% 
   group_split()
 
+
+# CHeck how many stands I have stands I have at each landscape? are stands id consistent over time for scenario???
+# Get number of of unique id by landscape
+for (i in seq_along(land.ls)) {print(length(unique(land.ls[[i]]$id)))}
+
+
+# Is the number of stands consisent by scenarios?
+df %>% 
+  group_by(scenario, year) %>% 
+  tally() %>% 
+  print(n = 1500)
+
+
+# Get number of scenarios
+length(unique(df$scenario))
+
+
+
+
+
 # object.size(land.ls.sel) # object with subesetted columns is 12 times smalled than with all columns!!!
 # ---------------------------------------------------
 # Calculate the pairs of neighbors:
@@ -79,6 +119,77 @@ merged.ls <- Map(merge, land.ls, open_edge.ls, by="id")
 # Merge partial dfs into one
 merged.df <- do.call(rbind, merged.ls)
 
+
+# CHeck the number of stands by landscape
+merged.df %>% 
+  group_by(landscape) %>% 
+  tally() %>% 
+  print(n = 1500)
+
+# 1472, seems correct
+
+
 # write the table
-fwrite(merged.df, "C:/MyTemp/myGitLab/windDamage/output/df_landscape_open_edge.csv")
+fwrite(merged.df, "C:/MyTemp/myGitLab/windDamage/output/even_flow/df_landscape_open_edge.csv")
+
+
+
+# Create a table to record landscape type, centrail id, central_h, neighbors and neighbors_H
+
+# ---------------
+# continue form here to get the height differences between stands
+df.sim = land.ls[[4]]
+nbrs[[4]]
+
+
+
+
+get_nbrs_H <- function(nbrs, 
+                       df.sim,...) {
+  require(sf)
+  require(dplyr)
+  
+  # Make a list to store outputs
+  nbrs_ls <- list()
+  
+  
+  for (i in seq_along(nbrs)) {
+
+    # get ids of the central stand and neighbors
+    central_id <- as.character(unique(nbrs[[i]]$central))
+    # Get neighbors
+    nbrs_id    <- unique(nbrs[[i]]$nbrs)
+  
+   # Get the stand height from simulated data for central stand and 
+    # neighbors 
+    central_H = rep(subset(df.sim, id %in% central_id)$H_dom, 
+                    length(nbrs_id))
+    nbrs_H    = subset(df.sim, id %in% nbrs_id)$H_dom
+    landscape = subset(df.sim, id %in% nbrs_id)$landscape
+    #print(nbrs_H)
+    nbrs_ls[[i]] <- data.frame(landscape,
+                               central_id, 
+                               central_H, 
+                               nbrs_id, 
+                               nbrs_H)
+  
+  df<- as.data.frame(do.call("rbind", nbrs_ls))
+  #  names(df) <- c("id", "open_edge")
+  return(df)
+  }
+}
+
+
+# get difference between neighbors on one landscape ----------------------
+nbrs.ls <- lapply(land.ls, function(df) get_nbrs_H(nbrs, df))
+  
+# Merge partial dfs into one
+merged.nbrs.df <- do.call(rbind, nbrs.ls)
+  
+# write the table
+fwrite(merged.nbrs.df, "C:/MyTemp/myGitLab/windDamage/output/even_flow/df_nbrs_diff.csv")
+  
+  
+  
+  
 
