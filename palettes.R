d455b774a8c2c92e82562a8f7d0b341b63bfb059
Potterf <mpotterf@jyu.fi>
2021-08-24 10:08:10 +0300

    list color palettes

---
+++ palettes.R
@@ -0,0 +1,17 @@
+
+
+# Color palettes
+
+# Color shades  
+cols_red = c('#F6BDC0', '#F07470', '#DC1C13')
+
+# Yeallow to red color palette
+#https://www.color-hex.com/color-palette/8727
+cols_ylRd5 <- c('#ff0000', # red
+                '#ff5a00', 
+                '#ff9a00', 
+                '#ffce00', 
+                '#f0ff00') # yellow
+cols_ylRd3 <- c(	'#ff9a00', # yellow 
+                 '#ff5a00', # orange
+                 '#ff0000' ) #red
