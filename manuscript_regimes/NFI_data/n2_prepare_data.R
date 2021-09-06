83f4bfc555e06e1dc206fe3dd15161e3379d1872
Potterf <mpotterf@jyu.fi>
2021-09-06 18:27:00 +0300

    renamed file

--- manuscript_regimes/NFI_data/n2_prepare_data.R
+++ manuscript_regimes/NFI_data/n2_prepare_data.R
@@ -220,51 +220,6 @@ df.ls2 <- lapply(names, getFiles)
 
 # Convert csv to db: will it reads faster?? --------------------------
 
-
-# ========================================
-#       Read data
-# ========================================
-
-library(DBI)
-
-# Create a new database:  
-# RSQLite: https://cran.r-project.org/web/packages/RSQLite/vignettes/RSQLite.html
-
-mydb <- dbConnect(RSQLite::SQLite(), "my-db.sqlite")
-
-# write my csv into database
-dbWriteTable(mydb, "df.no2", df.no2)
-
-# Check teh table
-dbListTables(mydb)
-
-dbDisconnect(mydb)
-
-
-
-# 
-# # Read all simulated data
-# dfs_input <- list.files(paste(inPath, inFolder, sep = "/"),
-#                        pattern = ".csv$")
-# (dfs_input)
-# # Change name to add place indicator:
-# dfs_names <- gsub("_rsu.csv", "", dfs_input)
-# dfs_names <- gsub("_MV_", "_", dfs_names) # actually, names are already included
-# 
-# # Read individual .csv
-# df.list <- lapply(dfs_input, function(name) data.table::fread(paste(inPath, inFolder, name, sep = "/"),
-#                                                data.table=FALSE, 
-#                                                stringsAsFactors = FALSE))
-
-
-
-# Read corrected simulated names: Korsnas
-# Unclear how to automate this, as needs to complete SOIL CLASS info?
-#name = "Korsnas"
-#fileName = paste( "without_MV_", name, '_rsu.csv', sep = "")
-#paste(inPath, inFolder, "without_MV_Korsnas_rsu.csv", sep = "/")
-# 
-
 # Make working example for no climate changes; then calculate values for CC scenario 
 rm(list = ls())
 
@@ -292,27 +247,140 @@ source("C:/MyTemp/myGitLab/windDamage/myFunctions.R")
 
 # get data
 # ----------------------
-inPath = "C:/MyTemp/2021_WindRisk_biodiversity/inputData/simulated"
+inPath = "C:/MyTemp/2021_WindRisk_biodiversity/inputData"
 
-system.time(df.no <- data.table::fread(paste(inPath, "rslt_FBE_rcp0.csv",  sep = "/"),  # 
-                       data.table=TRUE))
+# Storage info 
+in_folder  = "sim_filtered"
+out_folder = "output"
 
-# Changed naming just to check the file !! 
-#df.cc45 <- data.table::fread(paste(inPath, "rslt_FBE_rcp45.csv", sep = "/"))
-#df.cc85 <- data.table::fread(paste(inPath, "rslt_FBE_rcp85.csv", sep = "/"))
+# get vector of file names
+names = c("rslt_FBE_rcp0_f") #
+names = c("rslt_FBE_rcp45_f", "rslt_FBE_rcp85_f" )
+
+# Get raster values and filetr the raster vals
+# Get values for wind speed and temps sum
+df.raster <-   data.table::fread("C:/MyTemp/2021_WindRisk_biodiversity/inputData/spatial/df_raster_XY.csv",
+                                 data.table=TRUE)
+
+# Filter raster values
+df.raster <-  df.raster %>% 
+  dplyr::select(OBJECTID, 
+                ID, 
+                X_10, #Y, 
+                Stand, 
+                avgTemp, 
+                windSpeed)
+
+#df_name = "rslt_FBE_rcp0_f"
+
+addXYVals <- function(df_name, ...) {
+  #gc()
+
+    # Read over loop
+  #df_name = "rslt_FBE_rcp0_f"
+  in_name = paste0(df_name, '.csv')
+  out_name = paste0(gsub(pattern = "rslt_FBE_", 
+                         replacement = "", 
+                         x = df_name))
+
+  # Read simulated data 
+  df <- data.table::fread(paste(inPath, in_folder, in_name, sep = "/"),  # 
+                          data.table=TRUE)
+  
+  
+  
+  # ========================================== #
+  #      Create unique ID for simulated data   # --------------------------------
+  #            to merge them with XY           # --------------------------------
+  # ========================================== #
+  
+  # Steps: 
+  # 1. 'Zero paddling': add zeros to the beginning of 'id' , 
+  #     eg. fill in 00000XX values to have always 8 characters starting with 0
+  # 2. rename grid cells ('k3') into consecutive numbers ('1'...)
+  # 3. Add numeric grid indication at the beginning of the 8 digit paddled 'id'
+  # 4. voila! done
+  
+  df2 <- 
+    df %>% 
+    mutate(zero_id = formatC(id, 
+                             width = 8, 
+                             format = "d", 
+                             flag = "0")) %>% 
+    mutate(cell = str_sub(name,-2)) %>% 
+    mutate(nb = case_when( 
+      cell == "k3" ~ "1",
+      cell == "k4" ~ "2",
+      cell == "l2" ~ "3",
+      cell == "l3" ~ "4",
+      cell == "l4" ~ "5",
+      cell == "l5" ~ "6",
+      cell == "m3" ~ "7",
+      cell == "m4" ~ "8",
+      cell == "m5" ~ "9",
+      cell == "n3" ~ "10",
+      cell == "n4" ~ "11",
+      cell == "n5" ~ "12",
+      cell == "n6" ~ "13",
+      cell == "p3" ~ "14",
+      cell == "p4" ~ "15",
+      cell == "p5" ~ "16",
+      cell == "p6" ~ "17",
+      cell == "q3" ~ "18",
+      cell == "q4" ~ "19",
+      cell == "q5" ~ "20",
+      cell == "r4" ~ "21",
+      cell == "r5" ~ "22",
+      cell == "s4" ~ "23",
+      cell == "s5" ~ "24",
+      cell == "t4" ~ "25",
+      cell == "t5" ~ "26",
+      cell == "u4" ~ "27",
+      cell == "u5" ~ "28",
+      cell == "v3" ~ "29",
+      cell == "v4" ~ "30",
+      cell == "v5" ~ "31",
+      cell == "w3" ~ "32",
+      cell == "w4" ~ "33",
+      cell == "w5" ~ "34",
+      cell == "x4" ~ "35",
+      cell == "x5" ~ "36")) %>% 
+    mutate(ID = paste0(nb, zero_id)) %>% 
+    mutate(ID = as.character(ID))
+ 
+  rm(df)
+  
+ return(df2)
+}
+
+# Create and empty list to store the outputs
+my_ls <- ls()
+
+my_ls <- lapply(names, addXYVals) 
+
+
+# Read over loop
+df_name = "rslt_FBE_rcp0_f"
+in_name = paste0(df_name, '.csv')
+out_name = paste0(gsub(pattern = "rslt_FBE_", 
+                       replacement = "", 
+                       x = df_name))
+
+
+
+# Merge simulatd data with raster values 
+df <- data.table::fread(paste(inPath, in_folder, in_name, sep = "/"),  # 
+                        data.table=TRUE)
+
 
 # Get values for wind speed and temps sum
 df.raster <-   data.table::fread("C:/MyTemp/2021_WindRisk_biodiversity/inputData/spatial/df_raster_XY.csv",
                                  data.table=TRUE)
 
-
-
-
-
-
-# ==========================================
-#      Create unique ID for simulated data       --------------------------------
-# ==========================================
+# ========================================== #
+#      Create unique ID for simulated data   # --------------------------------
+#            to merge them with XY           # --------------------------------
+# ========================================== #
 
 # Steps: 
 # 1. 'Zero paddling': add zeros to the beginning of 'id' , 
@@ -321,8 +389,8 @@ df.raster <-   data.table::fread("C:/MyTemp/2021_WindRisk_biodiversity/inputData
 # 3. Add numeric grid indication at the beginning of the 8 digit paddled 'id'
 # 4. voila! done
 
-df.no2 <- 
-  df.no %>% 
+df2 <- 
+  df %>% 
   mutate(zero_id = formatC(id, 
                            width = 8, 
                            format = "d", 
@@ -367,132 +435,6 @@ df.no2 <-
     cell == "x5" ~ "36")) %>% 
    mutate(ID = paste0(nb, zero_id)) %>% 
   mutate(ID = as.character(ID))
-
-
-# ---------------------------------------------
-#   Get diagnostics
-# -------------------------------------------
-
-length(unique(df.no2$id)) # 54079
-
-
-unique(df.no2$branching_group)
-
-
-
-
-
-
-
-
-
-# -------------------------------------------------
-#   Made video??
-# ---------------------------------------------------
-
-# changing regimes over finland:
-# get NFI geometry
-# link datatable for one regime over years
-# color the code
-
-require(sf)
-df.geom <- sf::st_read("C:/MyTemp/2021_WindRisk_biodiversity/output", 
-                       layer = "XY_UTM_35")
-
-
-# plot with ggplot; ggplot is quite slow
-library(ggplot2)
-ggplot(df.geom) + geom_sf(aes(fill = Luokka))
-
-
-# subset one regimes for a nice attribute table
-
-df.bau<-
-  df.no2 %>% 
-  filter(regime == "BAU") %>% 
-  mutate(regime = factor(regime))  #%>%       # drop unused factors
-
-library(dplyr)
-df.bau <- df.bau %>% 
-  mutate(ID = as.character(id)) %>% 
-  dplyr::select(id)
-
-# remove the large file
-rm(df.no)
-rm(df.no2)
-
-# JOin geometry with simulated data: only one regime
-df.all<-
-  df.geom %>% 
-  mutate(ID = as.character(ID)) %>% 
-  left_join(df.bau, by = c("ID")) 
-
-
-
-# plot data
-library(ggspatial)
-theme_set(theme_bw())
-
-
-windows()
-df.all %>% 
-  filter(year == 2021) %>% 
-  ggplot() + 
-  geom_sf(aes(color = H_dom))  + # , size = 0.5 , size = AREA
-  scale_color_continuous(low = "lightgreen", 
-                        high = "darkgreen",
-                        space = "Lab", 
-                        na.value = "red", guide = "colourbar")#+
-
-
-
-
-
-# R animate: 
-
-
-# Get data:
-library(gapminder)
-
-# Charge libraries:
-library(ggplot2)
-library(gganimate)
-library(transformr)
-
-
-ggplot(df.all) + 
-  geom_sf(aes(color = Age)) + # , size = Age, size = 0.8size by factor itself!
-  scale_color_continuous(low = "lightgreen", 
-                         high = "black",
-                         space = "Lab", 
-                         na.value = "red", 
-                         guide = "colourbar")+
-  
-  annotation_scale(location = "bl", width_hint = 0.4) +
- # annotation_north_arrow(location = "bl", which_north = "true", 
-  #                       pad_x = unit(0.75, "in"), pad_y = unit(0.5, "in"),
-   #                      style = north_arrow_fancy_orienteering) +
-  theme_bw() +
-  xlab("Longitude") + 
-  ylab("Latitude") +
-  # theme(axis.title=element_blank(),
-  #      axis.text=element_blank(),
-  #     axis.ticks=element_blank()) +
-  # gganimate specific bits:
-  labs(title = 'Tree Age BAU Year: {current_frame}') +
-  transition_manual(year) +
-  #transition_time(year) +
-  ease_aes('linear')
-
-
-anim_save("NFI_Age.gif")
-# zac. 15:42~ cca 15 min to make one!
-
-
-
-
-
-
 
 
 
@@ -542,54 +484,6 @@ df.ls <- list(df.no, df.cc45, df.cc85)
 df.ls <- lapply(df.ls, function(df) df %>%  right_join(df.raster, 
                                                         by = c('id' = 'standid')))
 
-
-# Get initial year: 2015 based on SA values in 2016 ------------------------
-
-
-
-
-
-
-
-# Update a function
-addInitialYear <- function(df, ...) {
-  # Add first year to simulated data, getting the SA values
-  # for all regimes
-  # change it for 2015
-  library(tidyr)
-  
-#  df <- df.ls[[1]] 
-  
-  df.sa <- df %>% 
-    filter(regime == "SA_DWextract" & year == 2016) %>% 
-    mutate(year = 2015) %>%    # change the year indication
-    dplyr::select(-c(regime))  # remove column regime
-  
-  # get indication of column order in original data
-  col_names = names(df)
-  
-  # get vector of regimes  
-  regime_v <- 
-    df %>% 
-    distinct(regime) %>% 
-    pull(regime)
-  
-  # rename the column and reorder columns in sa_2015
-  #df.sa.out <- 
-  df.out <-
-    df.sa %>% 
-    crossing(regime_v) %>% # merge dataframe with vector of regime names 
-    rename(regime = regime_v) %>%
-    dplyr::bind_rows(df)  # merge data to new column, colums sort out by names
- 
-  return(df.out)
-}
-
-
-#aa<- addInitialYear(df.ls[[1]])
-
-
-df.ls.ini <- lapply(df.ls, addInitialYear)
 
 
 #df.ls.ini[[1]] %>% 
@@ -655,25 +549,6 @@ df.out <- do.call(rbind, df.risk.ls)
 
 
 
-
-# Testing -------------------------------------------------------------------
-
-# do differences in climate change exist here already??
-# yes, input data are correct. need to filter for excessive values
-#df.test <- do.call(rbind, df.ls)
-
-df.ls[[3]] %>% 
-  #group_by(name) %>% 
-  dplyr::select(name, H_dom) %>% 
-  summary()
-
-
-
-# which stands have unrealistic H_dom values?
-df.test %>% 
-  #filter(H_dom > 45430)
-  filter(H_dom > 100) %>% 
-  distinct(id, name, regime)
 
 
 
