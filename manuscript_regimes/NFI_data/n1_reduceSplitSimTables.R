b84152ea55a645f168581e0cc486898142625b2e
Potterf <mpotterf@jyu.fi>
2021-09-09 14:32:07 +0300

    added split function to make data smaller: spplit one data frame into multiple small ones

--- manuscript_regimes/NFI_data/n1_reduceSplitSimTables.R
+++ manuscript_regimes/NFI_data/n1_reduceSplitSimTables.R
@@ -6,11 +6,13 @@
 
 # simulated data has now BE (bionergy) alternative
 # to keep only the data that are withou bioenergy 'no_BE'
+# split tables into smaller ones by regime name
 
 # Process:
 
 # read data
 # filter them to exclude BE alternative
+# split data by regimes names to speed up processing
 # export data in new file
 
 
@@ -19,56 +21,59 @@ rm(list = ls())
 gc()
 
 
-setwd("C:/MyTemp/myGitLab/windDamage")
+#setwd("C:/MyTemp/myGitLab/windDamage")
 
 # Read libraries
-library(ggplot2)
 library(dplyr)
 library(tidyr)
-library(sf)
-library(rgdal)
-library(dplyr)
-library(spData)
-library(sf)
-library(stringr)
+library(data.table)
 
 
 # get data  --------------------------------------------------------
 inPath = "C:/MyTemp/2021_WindRisk_biodiversity/inputData"
 
-in_folder  = "simulated"
-out_folder = "sim_filtered"
+in_folder  = "sim_filtered"
+out_folder = "output_csv"
 
-names = c("rslt_FBE_rcp0", "rslt_FBE_rcp45", "rslt_FBE_rcp85") # 
+names = c("rslt_FBE_rcp0_f", "rslt_FBE_rcp45_f", "rslt_FBE_rcp85_f") # "rslt_FBE_rcp45_f", "rslt_FBE_rcp85_f" 
 
 # Define a function
 remove_BE <- function(df_name, ...) {
 
     # Name input variables
   #print(df_name)
+  #df_name = "rslt_FBE_rcp0_f"
   in_name    = paste0(df_name, ".csv")
+  print(in_name)
   
   # Name output variables
-  out_name   = paste0(df_name, "_f", ".csv") # _f as filtered, e.g. no BE scenarios
+  out_name   = gsub("rslt_FBE_", '', df_name)
+  out_name   = gsub("_f", '', out_name)
+  #out_name   = paste0(df_name, "_f", ".csv") # _f as filtered, e.g. no BE scenarios
  
+  print(out_name)
   
   # process the data  -------------------------------------------------
   df <- data.table::fread(paste(inPath, in_folder, in_name, sep = "/"),  # 
                           data.table=TRUE)
   
-  # Keep only if contains 'no_BE': how many regimes do i have left?
-  dd<- df %>% 
-    filter(grepl('no_BE', name)) 
+    # Keep only if contains 'no_BE': how many regimes do i have left?
+  #dd<- df %>% 
+   # filter(grepl('no_BE', name)) 
   
   
-  # export data -------------------------------------------------------
-  data.table::fwrite(dd,
-                     paste(inPath, out_folder, out_name, sep = "/"))
+  # split data and export -------------------------------------------------------
+  print('export data')
+  #(paste(inPath, paste0(out_name, regime,".csv")))
+  df[, fwrite(.SD, paste(inPath, out_folder, paste0(out_name, regime,".csv"), sep = "/")), by = regime]
+  
+  #data.table::fwrite(dd,
+   #                  paste(inPath, out_folder, out_name, sep = "/"))
   
   
   # Remove the tables -------------------------------------------------
   rm(df)
-  rm(dd)
+  #rm(dd)
 }
 
 lapply(names, remove_BE)
