acad50c839faf64a4f81795f49fccd0066111447
Potterf <ge45lep@mytum.de>
2022-03-09 10:44:31 +0100

    calculate NPV values for the individual stands, regimes and climate change

--- manuscript_regimes_biodiversity/NFI_data/n8_calculate_NPV.R
+++ manuscript_regimes_biodiversity/NFI_data/n8_calculate_NPV.R
@@ -1,31 +1,11 @@
 
+# ---------------------------------------
+#     Calculate NPV values  
+# ---------------------------------------
 
-# Calculate NPV values
 
 # calculate NPV for individual stands over time
-
-# from Kyle:
-
-# import pandas as pd
-# path = "/scratch/project_2003638/MAYA_risk/"
-# files = ["rcp0BAUwT_GTR_rsk"]
-# for file in files:
-#   t = pd.read_csv(path + file + ".csv")
-# t["disc_income"] = t['cash_flow']/((1.03)**(t['year']-2016))
-# t['disc_PV']=t['PV']/((1.03)**(t['year']-2016))
-# 
-# def age_groups(vec):
-#   x = vec[0]
-# y = vec[1]
-# if y < 2111:
-#   return 0
-# else:
-#   return x
-# t['disc_PV'] = t[['disc_PV','year']].apply(age_groups,axis = 1)
-# pd.DataFrame(t.groupby(['id','regime']).sum()['disc_PV']+t.groupby(['id','regime']).sum()['disc_income']).rename(columns={0: "NPV"}).to_csv(path+file+"_NPV.csv")
-
-
-
+# over id, climchange and regime 
 
 # Read data and make tables and plots
 rm(list = ls())
@@ -45,42 +25,82 @@ library(stringr)
 library(ggplot2)
 library(ggpubr)
 
-# Correct order of variables, factors & levels
-# need to have same names of columns?? YES
-# when data are sourced (source()), they are all available in my script
 source(paste(myPath, 'r_winddamage', 'myFunctions.R', sep = "/"))
 
 
 
-# Get data & final table ------------------------------------------------------------------------------
-inPath   = myPath
-inFolder = "output/windRisk_csv"
-inName   = 'rcp0BAUwT_GTR_rsk.csv'
+# Get data ------------------------------------------------------------------------------
+inPath    = myPath
+inFolder  = "output/windRisk_csv"
+outFolder = 'output/plotting'
+outName   = 'df_NPV.csv'
 
-# Input table
-df <- data.table::fread(paste(inPath, inFolder, inName,  sep = "/"),  # 
-                            data.table=TRUE, 
-                            stringsAsFactors = FALSE)
-                            
 
-# Make a function to calculate NPV:
+# Select only regimes of interest:   pattern="xx1|xxx2", 3 CC
+df.names = list.files(paste(inPath, inFolder, sep = "/"), 
+                      pattern = "CCF|x4|x3|x2|x1|BAUwoT_|BAUwGTR_|BAU_") # .csv$
+(df.names)
 
-calculate_NPV <- function(df, ...) {
-  # Calculate discounted income and PV 
-  df$disc_income = df$cash_flow/(1.03^(df$year-2016))
-  df$disc_PV = df$PV/(1.03^(df$year-2016))
-  
-  # Replace the disc_PV value by 0 if less year < 2111
-  df <- df %>% 
-    mutate(disc_PV = case_when(year < 2111 ~ 0,
-                                year >= 2111 ~ disc_PV))
-  # Calculate sums 
-  df <- df %>% 
-    group_by(id, regime) %>% 
-    summarize(NPV = sum(disc_PV, na.rm = T)+
-                sum(disc_income, na.rm = T))
-  
-  return(df) 
-}
+# Read dataframes
+df.ls <- lapply(df.names, function(name, ...) data.table::fread(paste(inPath, inFolder, name,  sep = "/"),  # 
+                                                                data.table=TRUE, 
+                                                                stringsAsFactors = FALSE,
+                                                                integer64="character"))
 
-df1<-calculate_NPV(df.out)
+# remove unnecessary columns
+cl_keep <- c(
+  "year",
+  "branching_group",
+  "Age",
+  "PV",
+  "cash_flow",
+  "name",
+  "cell",
+  "id"
+)
+
+
+# keep only needed columns
+df.ls2 <- lapply(df.ls, function(df) df %>% 
+                   dplyr::select(cl_keep))
+
+# Calculate the NPV
+df.ls2 <- lapply(df.ls2, calculate_NPV)
+
+
+# Merge data together -----------------------------------------------
+df.out <- do.call(rbind, df.ls2)
+
+
+# Correct regimes names --------------------------------------------------
+df.out <- df.out %>%
+  ungroup() %>% 
+  mutate(regime = case_when(
+    branching_group == 'Tapio harvest'                    ~ 'BAU', 
+    branching_group == 'Tapio harvest nature scene'       ~ 'GTR', 
+    branching_group == 'Tapio harvest without thinnings'  ~ 'noThin', 
+    branching_group == 'Long rotation harvest 10 p'       ~ 'ext_10', 
+    branching_group == 'Long rotation harvest 30 p'       ~ 'ext_30', 
+    branching_group == 'Short rotation harvest 30 n'      ~ 'short_30', 
+    branching_group == 'Short rotation harvest 10 n'      ~ 'short_10',
+    branching_group == 'Selection cut'                    ~ 'CCF')) 
+
+
+# Change order of change time---------------------------------------------
+df.out$climChange <-factor(df.out$climChange, 
+                           levels = c("REF", "RCP45", "RCP85"))
+
+
+
+# Check final table -------------------------------------------------------
+df.out <- df.out %>% 
+  ungroup() %>% 
+  select(-c(branching_group))
+
+
+
+# Export final table ------------------------------------------------------
+data.table::fwrite(df.out, paste(inPath, outFolder, outName, sep = "/"))
+
+
+
